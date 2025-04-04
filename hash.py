37a27b989adea7a6d8ecd5e262df67f12a9ddd30
Vladyslav Sokolovskyi <vladyslav.sokolovskyii@gmail.com>
2025-04-05 01:03:57 +0200

    Removed cache and log files from Git tracking

--- hash.py
+++
@@ -1,8 +0,0 @@
-import hashlib
-
-def hash_file(filepath, algo):
-    h = getattr(hashlib, algo)()
-    with open(filepath, 'rb') as f:
-        for chunk in iter(lambda: f.read(8192), b''):
-            h.update(chunk)
-    return h.hexdigest()
