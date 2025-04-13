006b10d13ca03cefc57568a221cda029887dcf08
Vladyslav Sokolovskyi <vladyslav.sokolovskyii@gmail.com>
2025-04-13 23:34:19 +0200

    rm source and replica folder. add automation create sorce and replica for test

--- conftest.py
+++ conftest.py
@@ -2,15 +2,33 @@ import pytest
 import os
 from test.utils import Fake_data
 from sync_tool.logger import setup_logging
+import logging
+import shutil
 setup_logging("test_log.log")
 
 
 @pytest.fixture
 def setup_test_data():
+
+   source = "source_test"
+   replica = "replica_test"
+   
+   for path in( source, replica):
+      if os.path.exists(path):
+         shutil.rmtree(path)
+         logging.info(f"Removed existing directory: {path}")
+      os.makedirs(path)
+      logging.info(f"Created directory: {path}")
+
    a = Fake_data()
-   a.fake_data_create("source", 5)
-   a.feke_folder_create("source",3,4)
-   yield
-   a.fake_data_delete("source")
-   a.fake_data_delete("replica")
+   a.fake_data_create("source_test", 5)
+   a.feke_folder_create("source_test",3,4)
+
+   yield source, replica
+   
+   a.fake_data_delete("source_test")
+   a.fake_data_delete("replica_test")
+
+   for path in (source,replica):
+      shutil.rmtree(path)
   
